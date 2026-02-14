b4ae3b24ce2007db1795f74989650800df79fe24
HANSINI2522 <hansinimohan2522@gmail.com>
2026-02-14 09:04:24 +0530

    my commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Login validation
+function loginUser() {
+  alert("Login functionality will connect to backend");
+}
+
+// Submit service request
+function submitRequest() {
+  alert("Service request submitted");
+}
